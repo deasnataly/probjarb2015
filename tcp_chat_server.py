543ab4cd36a0b78926c94eaef79baf823cac4577
ronayumik <rsns@localhost.localdomain>
2015-10-01 06:21:13 -0400

    tes tes

--- tcp_chat_server.py
+++ tcp_chat_server.py
@@ -16,34 +16,33 @@ def broadcast_data (sock, message):
  
 if __name__ == "__main__":
      
-    # List to keep track of socket descriptors
-    CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = 5000
+    # Nampung daftar koneksi dari client
+    CONNECTION_LIST = [] #array
+    RECV_BUFFER = 4096 #memory buffer
+    PORT = 5000 # port jalan
      
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("0.0.0.0", PORT))
-    server_socket.listen(10)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # supaya dapat digunakan berkali-kali address portnya
+    server_socket.bind(("0.0.0.0", PORT)) #listen localhost
+    server_socket.listen(10) #maksimum client
  
-    # Add server socket to the list of readable connections
+    #socket server diappend ke array
     CONNECTION_LIST.append(server_socket)
  
     print "Chat server started on port " + str(PORT)
  
-    while 1:
+    while 1: #listening connection
         # Get the list sockets which are ready to be read through select
         read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
  
         for sock in read_sockets:
-            #New connection
-            if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
-                sockfd, addr = server_socket.accept()
-                CONNECTION_LIST.append(sockfd)
+            #ada koneksi baru #asynchronous socket
+            if sock == server_socket: #checking kalau socketnya sesuai dengan socket server
+                sockfd, addr = server_socket.accept() # socket & addr client baru di-accept
+                CONNECTION_LIST.append(sockfd)  #push sockfd baru ke array
                 print "Client (%s, %s) connected" % addr
-                 
+                
                 broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
              
             #Some incoming message from a client
