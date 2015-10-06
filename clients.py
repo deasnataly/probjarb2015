52f092420c5f9091bcda55fd1625af8998e5e6d3
Ronauli S <deasnataly@gmail.com>
2015-10-06 01:15:54 -0400

    Adding backups

--- clients.py
+++ clients.py
@@ -1,55 +1,60 @@
-# telnet program example
+#telnet program example (?)
 import socket, select, string, sys
- 
+
 def prompt() :
-    sys.stdout.write('<You> ')
-    sys.stdout.flush()
- 
+	sys.stdout.write('<You> ')
+	sys.stdout.flush()
+	
 #main function
-if __name__ == "__main__":
-     
-    if(len(sys.argv) < 3) :
-        print 'Usage : python telnet.py hostname port'
-        sys.exit()
-     
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   # server_address = ('localhost', 5000)
-   # s.connect(server_address)
-    s.settimeout(2)
-     
-    # connect to remote host
-    try :
-        s.connect((host, port))
-    except :
-        print 'Unable to connect'
-        sys.exit()
-     
-    print 'Connected to remote host. Start sending messages'
-    prompt()
-     
-    while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
-        for sock in read_sockets:
-            #incoming message from remote server
-            if sock == s:
-                data = sock.recv(4096)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-                    #print data
-                    sys.stdout.write(data)
-                    prompt()
-             
-            #user entered a message
-            else :
-                msg = sys.stdin.readline()
-                s.send(msg)
-                prompt()
+if __name__ == "__main__" :
+	
+	if(len(sys.argv) < 3): #kalau jumlah kata yang di enter di python kurang dari 3
+		print ' Cara menggunakan : python clients.py <hostname> <port>\n'
+		sys.exit()
+		
+	host = sys.argv[1]
+	port = int (sys.argv[2])
+	
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	
+	s.settimeout(2)
+	
+	#connect ke remote host
+	try :
+		s.connect((host, port))
+	except :
+		print 'Tidak bisa konek, host anda mungkin tidak aktif atau salah port\n'
+		sys.exit()
+		
+	#sukses di try, masuk ke bawah
+	LIMIT = 4096
+	
+	
+	print 'Terkoneksi ke server. Ketik pesan anda!\n'
+	prompt() #masuk ke fungsi diatas
+	
+	
+	while 1:
+		list_socket = [sys.stdin, s]
+		#cari list socket yang readable
+		read_sockets, write_sockets, error_sockets = select.select(list_socket, [], [])
+		
+		for sock in read_sockets :
+			if sock == s : #ada pesan masuk dari server
+				data = sock.recv(LIMIT)
+				if not data :
+					print '\nTerputus dari server chat\n'
+					sys.exit()
+				else :
+					sys.stdout.write(data) #print data
+					prompt()
+				
+			else : #user memasukkan pesan
+				msg = sys.stdin.readline()
+				s.send(msg)
+				prompt()
+				
+					
+		
+		
+		
